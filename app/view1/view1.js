3571cc82114fd7c46fc045366fa931361ad7ce68
Xin Meng <mengxin.city@gmail.com>
2016-01-01 17:04:59 +0000

    Initial commit 1. Example page 45: directive.

---
+++ app/view1/view1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', [function() {
+}])
+.directive('myDirective', function() {
+  return {
+    restrict: 'E',
+    template: '<a href="http://google.co.uk"> ' +
+    'Click me to go to Google</a>'
+  };
+})
+;
