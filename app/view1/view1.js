a8de416326081001cbff7e3e32bf26518fd9bd55
Xin Meng <mengxin.city@gmail.com>
2016-01-01 17:07:01 +0000

    Page 45: directive  自定义指令案例

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,20 +2,21 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
-}])
-.directive('myDirective', function() {
-  return {
-    restrict: 'E',
-    template: '<a href="http://google.co.uk"> ' +
-    'Click me to go to Google</a>'
-  };
-})
+    .controller('View1Ctrl', [function () {
+    }])
+    //I add a directive here
+    .directive('myDirective', function () {
+        return {
+            restrict: 'E',
+            template: '<a href="http://google.co.uk"> ' +
+            'Click me to go to Google</a>'
+        };
+    })
 ;
